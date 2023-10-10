fbba3af24d84bae11e5c8060dfcbfb02701c3d52
Beatriz Cesconetto <108144622+Beacesconetto@users.noreply.github.com>
2023-10-10 20:24:33 -0300

    Create arduino_manipulando_1_led.ino

---
+++ arduino_manipulando_1_led.ino
@@ -0,0 +1,20 @@
+void setup()
+{
+	//Porta 13 (LED_BUILTIN) em modo de saída(escrita).
+	pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop()
+{
+	// Aguardamos 1000 milésimos de segundo
+	delay(1000); 
+	
+	// Escrevemos LOW(0) no pino digital 13
+	digitalWrite(LED_BUILTIN, LOW);
+	
+	// Aguardamos 1000 milésimos de segundo
+	delay(1000); 
+	
+	// Escrevemos HIGH(1) no pino digital 13
+	digitalWrite(LED_BUILTIN, HIGH);
+}
